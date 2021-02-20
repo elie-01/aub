70da75bc0eb460cc119ec3680bbf829098122f5d
elie-01 <79053303+elie-01@users.noreply.github.com>
2021-02-20 17:15:35 +0200

    Update udpclient.py

--- udpclient.py
+++ udpclient.py
@@ -4,7 +4,7 @@ import datetime
 s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 IP = "127.0.0.1"
 port = 50051
-
+x = 0
 for i in range (0,5):
 	msg = input("Send to server: ")
 	T1 = datetime.datetime.now()
@@ -13,6 +13,9 @@ for i in range (0,5):
 	T2 = datetime.datetime.now()
 	T3 = T2 - T1
 	print("data: ",str(data),"RTT = ",T3)
+	x = x + T3
 	
+RTTav = x/5
+print("RTTav")
    
 s.close()
